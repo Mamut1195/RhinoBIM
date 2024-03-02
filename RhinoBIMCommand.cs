0905113bdf07f08a8a0fc5f90637663d334c3b19
Mamut <100446119+Mamut1195@users.noreply.github.com>
2024-03-01 21:13:04 -0500

    Created Layer Function and GridX

--- RhinoBIMCommand.cs
+++ RhinoBIMCommand.cs
@@ -1,16 +1,19 @@
 ﻿using Rhino;
 using Rhino.Commands;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
+using Rhino.Render.ChangeQueue;
+using Rhino.Render.DataSources;
 using System;
 using System.Collections.Generic;
 
 namespace RhinoBIM
 {
-    public class RhinoBIMCommand : Command
+    public class GridX : Command
     {
-        public RhinoBIMCommand()
+        public GridX()
         {
             // Rhino only creates one instance of each command class defined in a
             // plug-in, so it is safe to store a refence in a static property.
@@ -18,50 +21,98 @@ namespace RhinoBIM
         }
 
         ///<summary>The only instance of this command.</summary>
-        public static RhinoBIMCommand Instance { get; private set; }
+        public static GridX Instance { get; private set; }
 
         ///<returns>The command name as it appears on the Rhino command line.</returns>
-        public override string EnglishName => "RhinoBIMCommand";
+        public override string EnglishName => "GridX";
 
+        
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
             // TODO: start here modifying the behaviour of your command.
             // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
+            RhinoApp.WriteLine("The {0} command will create a the grides in x direction", EnglishName);
 
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint())
+            // Solicitar al usuario el nombre del nuevo layer
+            GetString getLayerName = new GetString();
+            getLayerName.SetCommandPrompt("Enter the name of the new layer: ");
+            getLayerName.AcceptNothing(false);
+            getLayerName.Get();
+
+            if (getLayerName.CommandResult() != Result.Success)
+                return getLayerName.CommandResult();
+
+            string layerName = getLayerName.StringResult();
+
+            // Verificar si el layer existe
+            if (doc.Layers.FindName(layerName)!= null)
             {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt0 = getPointAction.Point();
+                // El layer ya existe
+                RhinoApp.WriteLine($"El layer '{layerName}' ya existe.");
+                return Result.Cancel;
             }
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
+            // Crear el nuevo layer
+            Layer newLayer = new Layer();
+            newLayer.Name = layerName;
+
+            // Añadir el nuevo layer al documento
+            int newIndex = doc.Layers.Add(newLayer);
+            if (newIndex < 0)
             {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DynamicDraw +=
-                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
+                RhinoApp.WriteLine("Error creating the new layer.");
+                return Result.Failure;
             }
 
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
+            RhinoApp.WriteLine($"New layer '{layerName}' created successfully.");
+      
 
-            // ---
-            return Result.Success;
+            using (GetNumber lenght = new GetNumber())
+            using (GetNumber spaces = new GetNumber())
+            using (GetNumber number_of_lines = new GetNumber())
+
+            {
+                lenght.SetCommandPrompt("Input length of the lines: ");
+                lenght.AcceptNumber(false, false);
+                lenght.Get();
+
+                spaces.SetCommandPrompt("Input spacing between lines: ");
+                spaces.AcceptNumber(false, false);
+                spaces.Get();
+
+                number_of_lines.SetCommandPrompt("Input number of lines: "); 
+                number_of_lines.AcceptNumber(false, false);
+                number_of_lines.Get();
+
+                if (lenght.CommandResult() == Result.Success && spaces.CommandResult() == Result.Success && number_of_lines.CommandResult() == Result.Success)
+                {
+                    double lineLength = lenght.Number();
+                    double spacing = spaces.Number();
+                    int numberOfLines = (int)number_of_lines.Number();
+
+                    Line Linea1 = new Line(0, 0, 0, lineLength, 0, 0);
+                    
+               
+                    doc.Objects.AddLine(Linea1);
+
+                    for (int i = 1; i < numberOfLines; i++) 
+                    {
+                        double newY = i * spacing; 
+                        Line newLine = new Line(0, newY, 0, lineLength, newY, 0);
+                        doc.Objects.AddLine(newLine);
+                    }
+
+                    doc.Views.Redraw();
+
+
+                }
+
+
+            }
+
+
+
+                return Result.Success;
         }
     }
 }
